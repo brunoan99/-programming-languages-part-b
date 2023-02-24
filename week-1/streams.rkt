fced726e24e0c0d932948d698559e63f6fa49824
Bruno Andrade <brunoan99@gmail.com>
2023-02-24 05:14:43 -0300

    Using Streams

---
+++ week-1/streams.rkt
@@ -0,0 +1,45 @@
+#lang racket
+
+(provide (all-defined-out))
+
+#| Using Streams |#
+
+#| Streams
+
+- A stream is an infinite sequence of values
+  - So cannot make a stream by making all the values
+  - Key idea: Use a thunk to delay creating most of the sequence
+  - Just a programming idiom
+
+A powerful concept for division of labor:
+  - Stream producer knows how create any number of values
+  - Stream consumer decides how many values to ask for
+
+Some examples of stream you might (not) be familiar with:
+  - User actions (mouse cliks, etc.)
+  - UNIX pipes: cmdl1 | cmd2 | has cmd2 "pull" data from cmd1
+  - Output values from a sequential feedback circuit
+
+|#
+
+#| Using streams
+
+We will represent streams using pairs and thunks
+
+Let a stream be a thunk that when called returns a pair:
+  '(next-answer . next-thunk)
+
+So given a stream st, the client can get any number of elements
+  - First:  (car (s))
+  - Second: (car ((cdr (s))))
+  - Third:  (car ((cdr ((cdr (s))))))
+(Usually bind (cdr (s)) to a variable or pass to a recursive function)
+
+|#
+(define (number-untill stream tester)
+  (letrec ([f (lambda (stream ans)
+                (let ([pr (stream)])
+                  (if (tester (car pr))
+                    ans
+                    (f (cdr pr) (+ ans 1)))))])
+      (f stream 1)))
