17baa97cd313a3c73d171a58cf7caff2b107a71a
Bruno Andrade <brunoan99@gmail.com>
2023-02-25 02:27:48 -0300

    Optional: Racket Macros With define-syntax

--- week-1/macros.rkt
+++ week-1/macros.rkt
@@ -50,6 +50,8 @@ It is like we added keywords to our language
   - Rewriting ("expansion") happens before execution
 |#
 
+#| Define-syntax |#
+
 ; a cosmetic macro -- adds then, else
 (define-syntax my-if
   (syntax-rules (then else)
@@ -61,11 +63,69 @@ It is like we added keywords to our language
   (syntax-rules ()
     [(comment-out ignore instead) instead]))
 
-; makes it so users don't write the thunk when using my-delay
+
+#| Revisiting delay and force
+
+Recall our definition of promises from earlier
+  - Should we use a macro instead to avoid clients' explicit thunk
+|#
+
+(define (my-delay1 th)
+  (mcons #f th))
+
+(define (my-force1 p)
+  (if (mcar p)
+     (mcdr p)
+     (begin (set-mcar! p #t)
+            (set-mcdr! p ((mcdr p)))
+            (mcdr p))))
+
+#|
+  (f (my-delay1 (lambda () e)))
+
+  (define (f p)
+    (... (my-force1 p) ...))
+|#
+
+#| A delay macro
+
+- A macro can put an expression under a thunk
+  - Delays evaluation without explicit thunk
+  - Cannot implement this with a function
+- Now client then should not use a thunk (that would double-thunk)
+  - Racket's pre-define delay is a similar macro
+
+makes it so users don't write the thunk when using my-delay
+|#
 (define-syntax my-delay
   (syntax-rules ()
     [(my-delay e)
      (mcons #f (lambda () e))]))
+#|
+  (f (my-delay e))
+|#
+
+; really bad because it evaluates e multiple times
+(define-syntax my-force-macro1
+  (syntax-rules ()
+    [(my-force e)
+      (if (mcar e)
+          (mcdr e)
+          (begin (set-mcar! e #t)
+                 (set-mcdr! e ((mcdr e)))
+                 (mcdr e)))]))
+
+; do *not* do this either because a function is exactly what we want
+(define-syntax my-force-macro2
+  (syntax-rules ()
+    [(my-force e)
+     (let ([x e])
+      (if (mcar e)
+          (mcdr e)
+          (begin (set-mcar! e #t)
+                 (set-mcdr! e ((mcdr e)))
+                 (mcdr e))))]))
+; just use my-force as a function
 
 #| Overuse
 
@@ -74,4 +134,5 @@ Macros often deserve a bad reputation because they are often overused or used wh
 When in doubt, resist defining a macro?
 
 But they can be used well and the optional material should help
+
 |#
